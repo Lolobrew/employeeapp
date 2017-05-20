233529d1666145edebc5285b21ae81811991df29
Chris Scott <OrigChrisScott@gmail.com>
2017-05-20 13:20:37 -0700

    Error on Current Employee Push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,10 +14,58 @@
  var database = firebase.database();
 
 
+var listEmployees = function(snapshot) {
+	var test = database.ref()
+	console.log(test);
+}
 
 var addEmployee = function() {
+	event.preventDefault();
 	
+	var name = "";
+	var role = "";
+	var startDate = null;
+	var monthsWorked = null;
+	var monthlyRate = null;
+	var totalBilled = null;
+
+	name = $("#employeeName").val().trim();
+	role = $("#employeeRole").val().trim();
+	startDate = $("#employeeStartDate").val().trim();
+	monthlyRate = parseFloat($("#employeeMonthlyRate").val().trim());
+
+	// Push to DB
+	database.ref().push({
+			name: name,
+			role: role,
+			startDate: startDate,
+			monthlyRate: monthlyRate
+	});
+
+	// <--- Not Working 
+	database.ref().on("child_added", function(snapshot){
+		// Need logic written
+		name = snapshot.val().name;
+		role = snapshot.val().role;
+		startDate = snapshot.val().startDate;
+		monthsWorked = 0;
+		monthlyRate = snapshot.val().monthlyRate;
+		totalBilled = monthsWorked * monthlyRate;
+
+		var string = "<tr><td>" + name + "</td><td>" + role + 
+					"</td><td>" + startDate + "</td><td>" + monthsWorked + 
+					"</td><td>" + monthlyRate + "</td><td>" + totalBilled + "</td>";
+
+		$("<tbody>").append(string);
+
+	});
+
+	// Clear old values from input boxes
+	$(".form-control").val("");	
 }
 
 
- $("#addEmployeeButton").on("click", addEmployee());
+// Invoke listEmployees function
+listEmployees();
+
+$("#addEmployeeButton").on("click", addEmployee);
